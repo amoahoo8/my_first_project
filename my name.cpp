6e384a446e5ff693e92249f1be691b497905245e
amoahoo8 <oamoah008@st.ug.edu.gh>
2022-05-27 19:32:56 -0700

    my commit

---
+++ my name.cpp
@@ -0,0 +1,21 @@
+
+/* a program that displays my name */
+
+
+#include <iostream>
+#include <string>
+ 
+int main()
+{
+    std::string my_name;
+ 
+    std::cout << "Hello, Enter your name.\n ";
+    std::cin >> my_name;
+    std::cout << "Hello there, " << my_name
+      <<" Is your name!\n";
+}
+
+	
+	
+	
+
